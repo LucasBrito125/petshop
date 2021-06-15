fb8c5b904f1b03634e230d8fa6028bb5cf98196a
LucasBrito125 <lucasbritosilva125@hotmail.com>
2021-06-15 18:37:24 -0300

    teste 15/06

--- paginas/cadastroAnimal.php
+++ paginas/cadastroAnimal.php
@@ -7,7 +7,7 @@
         }
 
     $conexao= mysqli_connect('us-cdbr-east-03.cleardb.com','b93ac2ca02a47f','4428a355','heroku_bd928228b7e37d8') or die('Erro ao conectar ao banco de dados');
-    $query = mysql_query($conexao, "SELECT idCliente, nomeCliente, telefone FROM cliente order by nomeCliente");
+    $query = mysqli_query($conexao, "SELECT idCliente, nomeCliente, telefone FROM cliente order by nomeCliente");
      
 ?>
 
